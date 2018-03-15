0daed16da0b08c6b95143dfad545b6e36cee1118
VincentPelletier <vincent.pelletier.72440@gmail.com>
2018-03-15 18:01:00 +0100

    projet SBD

---
+++ Assainissement/src/APIJDBC/Connexion.java
@@ -0,0 +1,56 @@
+package APIJDBC;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Connexion {
+
+	public static void main(String[] argv) {
+
+		System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+		System.out.println("Mot de passe de la base : ");
+		Scanner sc = new Scanner(System.in);
+		String mdp = sc.next();
+		sc.close();
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost/base_projet_sbd", "postgres",
+					mdp);
+			
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+
+}
---
+++ Assainissement/src/APIJDBC/Statement.java
@@ -0,0 +1,5 @@
+package APIJDBC;
+
+public class Statement {
+
+}
